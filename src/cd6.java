218b75d41307a105a30999abab97a975586bcf19
GushchinN <gushchinmirea@mail.ru>
2026-09-21 22:41:12 +0300

    добавил фотки

--- src/cd6.java
+++ src/cd6.java
@@ -2,7 +2,7 @@ public class cd6 {
     public static void main(String[] args) {
 
         System.out.print("1 +");
-        int n = 100;
+        int n = 10;
         for(int i = 2; i < n; i++){
             if (i<n-1) {
                 System.out.print(" 1/" + i + " +");
